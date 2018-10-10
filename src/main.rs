1d35cf242651005f648ac6541ffb9fecb3a9a25e
Hampus Linander <hampus.linander@gmail.com>
2018-10-11 00:20:46 +0200

    Add dynamics with velocity verlet step
    Add main loop based on
    https://github.com/tensor-programming/wasm_snake_example
    Add debug log using inline js console.log.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
+#[macro_use]
 extern crate stdweb;
 
 use stdweb::web::html_element::CanvasElement;
 use stdweb::web::{self, INonElementParentNode, CanvasRenderingContext2d};
 use stdweb::unstable::TryInto;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 mod kernels;
 mod math;
 mod sph;
 
+const DT: f64 = 0.01;
+struct Canvas {
+    pub canvas: CanvasElement,
+    pub ctx: CanvasRenderingContext2d,
+    width: u32,
+    height: u32
+}
 
 fn main() {
     let tmp = web::document()
@@ -20,12 +30,23 @@ fn main() {
     let height = canvas.height();
     let ctx: CanvasRenderingContext2d = canvas
         .get_context().unwrap();
+    let canvasHolder = Canvas {
+        canvas,
+        ctx,
+        width,
+        height
+    };
     let mut state = sph::create_initial_state();
-    sph::update_density(&mut state);
-    for y in 0..height {
-        for x in 0..width {
-            let density = sph::density(&state, x as f64 / width as f64, y as f64 / height as f64);
-            let mut norm_density = (255. * (density - 0.6) / (0.7 - 0.6)).round() as i32;
+    main_loop(Rc::new(canvasHolder), Rc::new(RefCell::new(state)));
+}
+
+fn main_loop(canvas: Rc<Canvas>, state: Rc<RefCell<Vec<sph::Particle>>>) {
+    let max_density = sph::update_density(&mut state.borrow_mut());
+    sph::update_state(&mut state.borrow_mut(), DT);
+    for y in 0..canvas.height {
+        for x in 0..canvas.width {
+            let density = sph::density(&state.borrow(), x as f64 * 5.0 / canvas.width as f64, y as f64 * 5.0 / canvas.height as f64);
+            let mut norm_density = (255. * density / (max_density)).round() as i32;
             if norm_density > 255 {
                 norm_density = 255;
             }
@@ -33,8 +54,11 @@ fn main() {
                 norm_density = 0;
             }
             // let index = (y * width + x) * 4;
-            ctx.set_fill_style_color(&format!("rgb({}, 0, 0)", norm_density));
-            ctx.fill_rect(x as f64, y as f64, 1.0, 1.0);
+            canvas.ctx.set_fill_style_color(&format!("rgb({}, 0, 0)", norm_density));
+            canvas.ctx.fill_rect(x as f64, y as f64, 1.0, 1.0);
         }
     }
+    web::set_timeout(move || {
+        main_loop(canvas.clone(), state.clone());
+    }, 30);
 }
--- src/sph.rs
+++ src/sph.rs
@@ -1,16 +1,14 @@
-// extern crate num_traits;
-// mod math;
 use kernels;
 use math;
 
-// use num_traits::pow;
-
-const REST_DENS: f64 = 1000.0;
+const MAX_X: f64 = 5.0;
+const MIN_X: f64 = 0.0;
+const MAX_Y: f64 = 5.0;
+const REST_DENS: f64 = 5.0;
 const GAS_CONST: f64 = 2000.0;
-const H: f64 = 16.0;
+const H: f64 = 5.0;
 const M: f64 = 65.0;
-// const VISC: f64 = 250.0;
-// const DT: f64 = 0.0008;
+const MU: f64 = 250.0;
 
 #[derive(Clone)]
 pub struct Particle {
@@ -20,6 +18,8 @@ pub struct Particle {
     vy: f64,
     fx: f64,
     fy: f64,
+    ofx: f64,
+    ofy: f64,
     density: f64,
     pressure: f64
 }
@@ -34,6 +34,8 @@ fn create_particle(x_: f64, y_: f64) -> Particle {
         vy: 0.,
         fx: 0.,
         fy: 0.,
+        ofx: 0.,
+        ofy: 0.,
         density: 0.,
         pressure: 0.
     }
@@ -50,11 +52,11 @@ pub fn create_initial_state() -> Vec<Particle> {
     return particles;
 }
 
-pub fn update_density(particles: &mut Vec<Particle>) {
+pub fn update_density(particles: &mut Vec<Particle>) -> f64 {
     for particle in particles.iter_mut() {
         particle.density = 0.0;
     }
-
+    let mut max_density = 0.0;
     // Wat?!
     for i in 0..particles.len() {
         let mut density = 0.;
@@ -68,9 +70,80 @@ pub fn update_density(particles: &mut Vec<Particle>) {
         }
         {
             let particle = &mut particles[i];
-            particle.pressure = GAS_CONST*(particle.density-REST_DENS);
             particle.density = density;
+            if density > max_density {
+                max_density = density;
+            }
+            particle.pressure = GAS_CONST*(particle.density-REST_DENS);
         }
+    }
+    {
+        js! {
+            console.log("Max density: " + @{max_density});
+        }
+    }
+    return max_density;
+}
+
+pub fn update_state(particles: &mut Vec<Particle>, dt: f64) {
+    for particle in particles.iter_mut() {
+        particle.fx = 0.0;
+        particle.fy = 50.0;
+
+        // Velocity Verlet (position update)
+        particle.x = particle.x + particle.vx * dt + 0.5*(particle.fx / M) * dt * dt;
+        particle.y = particle.y + particle.vy * dt + 0.5*(particle.fy / M) * dt * dt;
+        if particle.x > MAX_X {
+            particle.x = MAX_X;
+            particle.vx = 0.0;
+        }
+        if particle.x < MIN_X {
+            particle.x = MIN_X;
+            particle.vx = 0.0;
+        }
+        if particle.y > MAX_Y {
+            particle.y = MAX_Y;
+            particle.vy = 0.0;
+        }
+    }
+
+    for i in 0..particles.len() {
+        let mut fx = 0.;
+        let mut fy = 0.;
+        {
+            let particle1 = &particles[i];
+            for j in 0..particles.len() {
+                let particle2 = &particles[j];
+                let rx = particle1.x - particle2.x;
+                let ry = particle1.y - particle2.y;
+                let p_over_rho_1 = particle1.pressure / math::pow(particle1.density, 2);
+                let p_over_rho_2 = particle2.pressure / math::pow(particle2.density, 2);
+                let (grad_x, grad_y) = kernels::grad_kernel_2d(rx, ry, H);
+                let advection = -M*(p_over_rho_1 + p_over_rho_2);
+                let diffusion = MU / particle1.density * M / particle2.density;
+                fx += grad_x * advection + diffusion * (particle2.vx - particle1.vx);
+                fy += grad_y * advection + diffusion * (particle2.vy - particle1.vy);
+            }
+        }
+        {
+            let particle = &mut particles[i];
+            particle.ofx = particle.fx;
+            particle.ofy = particle.fy;
+            particle.fx = fx;
+            particle.fy = fy;
+        }
+    }
+
+    {
+        let val = particles[0].vy;
+        js! {
+            console.log("Particle 1 vy: " + @{val});
+        }
+    }
+    for particle in particles.iter_mut() {
+        // Velocity Verlet (velocity update)
+        particle.vx = particle.vx + (particle.ofx + particle.fx) / M / 2.0 * dt;
+        particle.vy = particle.vy + (particle.ofy + particle.fy) / M / 2.0 * dt;
     }
 }
 
