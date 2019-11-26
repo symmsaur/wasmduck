686837e827b98d773e13d1e5aaeec5b6888d044a
Jonathan Karlsson <karlsjona@gmail.com>
2019-11-26 23:36:06 +0100

    Make water more watery

--- src/main_wasm.rs
+++ src/main_wasm.rs
@@ -5,7 +5,7 @@ extern crate webgl_stdweb;
 use stdweb::unstable::TryInto;
 use stdweb::web::html_element::CanvasElement;
 use stdweb::web::{self, INonElementParentNode, TypedArray};
-use webgl_stdweb::{WebGLRenderingContext as GL, ANGLE_instanced_arrays};
+use webgl_stdweb::{WebGLRenderingContext as GL, ANGLE_instanced_arrays, WebGLBuffer};
 
 mod grid;
 mod kernels;
@@ -26,7 +26,10 @@ const DT: f64 = 0.0005;
 struct Canvas {
     pub canvas: CanvasElement,
     pub ctx: GL,
+    pub ext: ANGLE_instanced_arrays,
+    pub offset_buffer: std::option::Option<WebGLBuffer>,
 }
+
 fn main() {
     let canvas: CanvasElement = web::document()
         .get_element_by_id("win")
@@ -42,16 +45,32 @@ fn main() {
 
     let level = 0;
     let internal_format = GL::RGBA as i32;
-    let width = 2;
-    let height = 2;
+    let width = 4;
+    let height = 4;
     let border = 0;
     let src_format = GL::RGBA;
     let src_type = GL::UNSIGNED_BYTE;
     let pixel_buf = [
-        255u8, 0, 0, 255u8,
-        0, 255u8, 0, 255u8,
-        0, 0, 255u8, 255u8,
-        255u8, 255u8, 255u8, 255u8,
+        // top row
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
+        // first middle row
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 64u8,
+        4u8, 4u8, 255u8, 64u8,
+        4u8, 4u8, 255u8, 32u8,
+        // second middle row
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 64u8,
+        4u8, 4u8, 255u8, 64u8,
+        4u8, 4u8, 255u8, 32u8,
+        // bottom row
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
+        4u8, 4u8, 255u8, 16u8,
     ];
     let pixel = TypedArray::<u8>::from(&pixel_buf[..]);
     ctx.tex_image2_d(
@@ -66,8 +85,8 @@ fn main() {
         Some(&pixel),
     );
 
-    ctx.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
-    ctx.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
+    // ctx.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MIN_FILTER, GL::NEAREST as i32);
+    // ctx.tex_parameteri(GL::TEXTURE_2D, GL::TEXTURE_MAG_FILTER, GL::NEAREST as i32);
 
     ctx.generate_mipmap(GL::TEXTURE_2D);
 
@@ -95,11 +114,10 @@ fn main() {
     let tex_coord_data = TypedArray::<f32>::from(&tex_coord_internal[..]).buffer();
     ctx.buffer_data_1(GL::ARRAY_BUFFER, Some(&tex_coord_data), GL::STATIC_DRAW);
 
+
     let offset_buffer = ctx.create_buffer();
     ctx.bind_buffer(GL::ARRAY_BUFFER, offset_buffer.as_ref());
-    let offset_internal  = [0.0, 0.2,
-                          0.5, 0.0,];
-    let offset_data = TypedArray::<f32>::from(&offset_internal[..]).buffer();
+    let offset_data = TypedArray::<f32>::from(&[0.0; sph::N_PARTICLES as usize * 2][..]).buffer();
     ctx.buffer_data_1(GL::ARRAY_BUFFER, Some(&offset_data), GL::STATIC_DRAW);
 
     // Create vertex shader
@@ -111,7 +129,7 @@ fn main() {
             varying highp vec2 vTextureCoord;
 
             void main(void) {
-                vec3 pos = position / 4.0 + vec3(offset, 0.0);
+                vec3 pos = position / 8.0 + vec3(offset, 0.0);
                 gl_Position = vec4(pos, 1.0);
                 vTextureCoord = aTextureCoord;
             }"#;
@@ -161,96 +179,39 @@ fn main() {
     ext.vertex_attrib_divisor_angle(offs, 1);
 
     ctx.use_program(Some(&shady_program));
+    ctx.enable(GL::BLEND);
+    ctx.blend_func(GL::SRC_ALPHA, GL::ONE);
 
-    // ctx.draw_elements(GL::TRIANGLES, 6, GL::UNSIGNED_SHORT, 0);
-    ext.draw_elements_instanced_angle(GL::TRIANGLES, 6, GL::UNSIGNED_SHORT, 0, 2);
+    ctx.clear_color(0.0, 0.0, 0.0, 1.0);
+
+    ext.draw_elements_instanced_angle(GL::TRIANGLES, 6, GL::UNSIGNED_SHORT, 0, sph::N_PARTICLES as i32);
+
+    let state = sph::create_initial_state();
+
+    // ctx.viewport(0, 0, width as i32, height as i32);
+    let canvas_holder = Canvas { canvas, ctx, ext, offset_buffer };
+    main_loop(canvas_holder, state, 0.0);
 }
-
-// fn main() {
-//     let canvas: CanvasElement = web::document()
-//         .get_element_by_id("win")
-//         .unwrap()
-//         .try_into()
-//         .unwrap();
-//     let width = canvas.width();
-//     let height = canvas.height();
-//     let ctx: GL = canvas.get_context().unwrap();
-
-//     let vertices = TypedArray::<f32>::from(&[0.0; sph::N_PARTICLES as usize * 2][..]).buffer();
-//     let vertex_buffer = ctx.create_buffer().unwrap();
-//     ctx.bind_buffer(GL::ARRAY_BUFFER, Some(&vertex_buffer));
-//     ctx.buffer_data_1(GL::ARRAY_BUFFER, Some(&vertices), GL::STATIC_DRAW);
-
-//     let mut vinst = [0u16; sph::N_PARTICLES as usize];
-//     for i in 0..sph::N_PARTICLES {
-//         vinst[i as usize] = i as u16;
-//     }
-//     let indices = TypedArray::<u16>::from(&vinst[..]).buffer();
-//     let index_buffer = ctx.create_buffer().unwrap();
-//     ctx.bind_buffer(GL::ELEMENT_ARRAY_BUFFER, Some(&index_buffer));
-//     ctx.buffer_data_1(GL::ELEMENT_ARRAY_BUFFER, Some(&indices), GL::STATIC_DRAW);
-
-//     // Create vertex shader
-//     let vert_shader_code = r#"
-//         attribute vec2 position;
-
-//         void main(void) {
-//             gl_Position = vec4(position, 0., 1.);
-//             gl_PointSize = 2.;
-//         }"#;
-//     let vert_shader = ctx.create_shader(GL::VERTEX_SHADER).unwrap();
-//     ctx.shader_source(&vert_shader, vert_shader_code);
-//     ctx.compile_shader(&vert_shader);
-
-//     // Create fragment shader
-//     let frag_shader_code = r#"
-//         void main(void) {
-//             gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-//         }"#;
-//     let frag_shader = ctx.create_shader(GL::FRAGMENT_SHADER).unwrap();
-//     ctx.shader_source(&frag_shader, frag_shader_code);
-//     ctx.compile_shader(&frag_shader);
-
-//     // Create shader program
-//     let shady_program = ctx.create_program().unwrap();
-//     ctx.attach_shader(&shady_program, &vert_shader);
-//     ctx.attach_shader(&shady_program, &frag_shader);
-//     ctx.link_program(&shady_program);
-
-//     // Associate attributes to shaders
-//     ctx.bind_buffer(GL::ARRAY_BUFFER, Some(&vertex_buffer));
-//     let pos = ctx.get_attrib_location(&shady_program, "position") as u32;
-//     ctx.vertex_attrib_pointer(pos, 2, GL::FLOAT, false, 0, 0);
-//     ctx.enable_vertex_attrib_array(pos);
-
-//     ctx.use_program(Some(&shady_program));
-
-//     let state = sph::create_initial_state();
-
-//     ctx.viewport(0, 0, width as i32, height as i32);
-//     let canvas_holder = Canvas { canvas, ctx };
-//     main_loop(canvas_holder, state, 0.0);
-// }
 
 fn main_loop(canvas: Canvas, mut state: sph::State, _dt: f64) {
     let (_grid, _debug) = sph::update_state(&mut state, DT, sph::SPHDebug::new());
 
-    let mut vertices_array = [0.0f32; sph::N_PARTICLES as usize * 2];
+    let mut offsets = [0.0; sph::N_PARTICLES as usize * 2];
     for i in 0..(sph::N_PARTICLES as usize) {
         let x = state.particles[i].x;
         let y = state.particles[i].y;
-        vertices_array[2 * i] =
+        offsets[2 * i] =
             ((((x - sph::MIN_X) / (sph::MAX_X - sph::MIN_X)) - 0.5) * 2.0) as f32;
-        vertices_array[2 * i + 1] =
+        offsets[2 * i + 1] =
             (((-(y - sph::MIN_Y) / (sph::MAX_Y - sph::MIN_Y)) + 0.5) * 2.0) as f32;
     }
-    let vertices = TypedArray::<f32>::from(&vertices_array[..]).buffer();
+    let offset_data = TypedArray::<f32>::from(&offsets[..]).buffer();
+    canvas.ctx.bind_buffer(GL::ARRAY_BUFFER, canvas.offset_buffer.as_ref());
     canvas
         .ctx
-        .buffer_data_1(GL::ARRAY_BUFFER, Some(&vertices), GL::STATIC_DRAW);
-    canvas
-        .ctx
-        .draw_elements(GL::POINTS, sph::N_PARTICLES as i32, GL::UNSIGNED_SHORT, 0);
+        .buffer_data_1(GL::ARRAY_BUFFER, Some(&offset_data), GL::STATIC_DRAW);
+    canvas.ctx.clear(GL::COLOR_BUFFER_BIT);
+    canvas.ext.draw_elements_instanced_angle(GL::TRIANGLES, 6, GL::UNSIGNED_SHORT, 0, sph::N_PARTICLES as i32);
     web::window().request_animation_frame(move |dt| {
         main_loop(canvas, state, dt);
     });
