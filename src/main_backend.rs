56415c05a6b4701eac82978fedf8e314ed2c1356
Jonathan Karlsson <karlsjona@gmail.com>
2020-06-14 19:57:22 +0200

    Serve wasm using actix backend

--- src/main_backend.rs
+++ src/main_backend.rs
@@ -1,13 +1,18 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_files::{Files, NamedFile};
+use actix_web::{web, App, HttpServer, Result};
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello Wasm!")
+async fn index() -> Result<NamedFile> {
+    Ok(NamedFile::open("static/index.html")?)
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/index.html", web::get().to(index))
+            .service(Files::new("/", "target/wasm32-unknown-unknown/release"))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
